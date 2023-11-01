3be5fe41fcf24c7a7df6a756f342fc11c53be54e
eleeando <andoulsielee@gmail.com>
2023-11-01 17:28:52 +0300

    ee

---
+++ WebFundamentals/Week 2/Day 1/Practice/profile-page/script.js
@@ -0,0 +1,9 @@
+function clicker(){
+   var element=document.querySelector('#mainname');
+   element.innerHTML = "Phil E";
+}
+
+var elementCard=document.querySelector('.card-list-item')
+function decrease(){
+    elementCard.remove()
+}
