218991eeb6f78c415971e163763924668da9e34c
unknown <andoulsielee@gmail.com>
2023-11-06 16:05:30 +0100

    work

--- WebFundamentals/Week 2/Day 1/Practice/profile-page/script.js
+++
@@ -1,9 +0,0 @@
-function clicker(){
-   var element=document.querySelector('#mainname');
-   element.innerHTML = "Phil E";
-}
-
-var elementCard=document.querySelector('.card-list-item')
-function decrease(){
-    elementCard.remove()
-}
