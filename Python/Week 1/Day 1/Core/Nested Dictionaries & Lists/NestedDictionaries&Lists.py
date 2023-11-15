0e9c97b63c976680d60a0f6362e714d2df1301c1
unknown <andoulsielee@gmail.com>
2023-11-15 16:03:58 +0100

    2nd day

--- Python/Week 1/Day 1/Core/Nested Dictionaries & Lists/NestedDictionaries&Lists.py
+++ Python/Week 1/Day 1/Core/Nested Dictionaries & Lists/NestedDictionaries&Lists.py
@@ -35,24 +35,24 @@
 #         print(key,"-", val) 
 
 #3- Get Values From a List of Dictionaries
-# students = [
-#         {'first_name':  'Michael', 'last_name' : 'Jordan'},
-#         {'first_name' : 'John', 'last_name' : 'Rosales'},
-#         {'first_name' : 'Mark', 'last_name' : 'Guillen'},
-#         {'first_name' : 'KB', 'last_name' : 'Tonel'}
-#     ]
-# for i in range (len(students)):
-#     print(students[i]['first_name'])
+students = [
+        {'first_name':  'Michael', 'last_name' : 'Jordan'},
+        {'first_name' : 'John', 'last_name' : 'Rosales'},
+        {'first_name' : 'Mark', 'last_name' : 'Guillen'},
+        {'first_name' : 'KB', 'last_name' : 'Tonel'}
+    ]
+for i in range (len(students)):
+    print(students[i]['first_name'])
 
 #4- Iterate Through a Dictionary with List Values
-dojo = {
-    'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
-    'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
-}
-def printInfo(arr):
-    for key, val in arr.items():
-        print(f"{len(val)} {key.upper()}")
-        for items in val:
-            print(items)
-printInfo(dojo)
+# dojo = {
+#     'locations': ['San Jose', 'Seattle', 'Dallas', 'Chicago', 'Tulsa', 'DC', 'Burbank'],
+#     'instructors': ['Michael', 'Amy', 'Eduardo', 'Josh', 'Graham', 'Patrick', 'Minh', 'Devon']
+# }
+# def printInfo(arr):
+#     for key, val in arr.items():
+#         print(f"{len(val)} {key.upper()}")
+#         for items in val:
+#             print(items)
+# printInfo(dojo)
 
